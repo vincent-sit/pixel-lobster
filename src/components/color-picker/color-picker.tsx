fbb36b99aa326d5ba42a52d1d3bbd9fcb7b5b83d
Vincent Sit <vincentsit00@ymail.com>
2024-01-17 22:31:00 +1300

    Add buttons to change color of pen

--- src/components/color-picker/color-picker.tsx
+++ src/components/color-picker/color-picker.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
+import { ColorContext } from '../../contexts/color-context';
 
 const Wrapper = styled.div`
     background-color: #D32A2A;    
 `;
 
 export function ColorPicker() {
+    const {updateColor} = useContext(ColorContext);
+
+    // create color picker and registers the color to the context so it can be used elsewhere
+
     return (
         <Wrapper>
-
+            <div>
+                <button onClick={() => {updateColor('red');}}>red</button>
+                <button onClick={() => {updateColor('yellow');}}>yellow</button>
+            </div>
         </Wrapper>
     );
 }
