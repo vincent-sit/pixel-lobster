9e9212fb2f1197c144669a7f9779197670f5077d
Vincent Sit <vincentsit00@ymail.com>
2024-01-17 19:03:10 +1300

    Implement canvas prototype

--- src/components/canvas/canvas.tsx
+++ src/components/canvas/canvas.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
@@ -8,9 +8,44 @@ const Wrapper = styled.div`
 `;
 
 export function Canvas() {
+    const [isDown, setIsDown] = useState(false);
+    const [ctx, setCtx] = useState<CanvasRenderingContext2D | null>(null);
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+
+    useEffect(() => {
+        if (!canvasRef.current) {
+            return;
+        }
+
+        const currCtx = canvasRef.current.getContext('2d');
+        if (currCtx) {
+            setCtx(currCtx);
+            currCtx.fillStyle = 'blue';
+            currCtx.fillRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+        }
+    }, []);
+
+    function handleDown() {
+        setIsDown(true);
+    }
+
+    function handleUp() {
+        setIsDown(false);
+    }
+
+    function handleMove(e: React.PointerEvent<HTMLCanvasElement>) {
+        if (!isDown || !ctx || !canvasRef.current) {
+            return;
+        }
+
+        const rect = canvasRef.current.getBoundingClientRect();
+        ctx.fillStyle = 'black';
+        ctx.fillRect(e.clientX - rect.x, e.clientY - rect.y, 1, 1);
+    }
+
     return (
         <Wrapper>
-
+            <canvas ref={canvasRef} width="300" height="300" onPointerDown={handleDown} onPointerUp={handleUp} onPointerMove={handleMove}></canvas>
         </Wrapper>
     );
 }
