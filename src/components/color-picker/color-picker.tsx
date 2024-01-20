120a60bdfb22493d210f1fffc56a9e120e71fbc2
Tommy <51040159+tommyquant@users.noreply.github.com>
2024-01-20 22:09:52 +1300

    Clean up color picker code

--- src/components/color-picker/color-picker.tsx
+++ src/components/color-picker/color-picker.tsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
+import Color from 'colorjs.io';
 import { ColorContext } from '../../contexts/color-context';
 import { usePointer } from '../../hooks/use-pointer';
 
@@ -22,8 +23,7 @@ interface Coord {
 }
 
 export function ColorPicker() {
-    const {color, updateColor} = useContext(ColorContext);
-    const [coreColor, setCoreColor] = useState('red');
+    const { color, updateColor } = useContext(ColorContext);
     const [canvasCtx, setCanvasCtx] = useState<CanvasRenderingContext2D | null>(null);
     const [sliderCtx, setSliderCtx] = useState<CanvasRenderingContext2D | null>(null);
     const colorCanvasRef = useRef<HTMLCanvasElement>(null);
@@ -31,55 +31,59 @@ export function ColorPicker() {
     const colorSelectedRef = useRef<HTMLDivElement>(null);
     const colorCoordCircleRef = useRef<HTMLCanvasElement>(null);
     const [selectionCoord, setSelectionCoord] = useState<Coord>({x: 0, y: 0});
-    const { isDown, x: pointerX, y: pointerY } = usePointer(colorCanvasRef);
+    const { isDown: isColorDown, x: colorX, y: colorY } = usePointer(colorCanvasRef);
+    const { y: hueY } = usePointer(colorSliderRef);
 
-    // set color canvas
-    useEffect(() => {
+    function updateColorCanvas(hue: number) {
         if (!colorCanvasRef.current) return;
 
         const currCanvasCtx = colorCanvasRef.current.getContext('2d');
-        if (currCanvasCtx) {
-            setCanvasCtx(currCanvasCtx);
-            
-            const gradientH = currCanvasCtx.createLinearGradient(0, 0, currCanvasCtx.canvas.width, 0);
-            gradientH.addColorStop(0, '#fff');
-            gradientH.addColorStop(1, coreColor);
-            currCanvasCtx.fillStyle = gradientH;
-            currCanvasCtx.fillRect(0, 0, currCanvasCtx.canvas.width, currCanvasCtx.canvas.height);
-
-            const gradientV = currCanvasCtx.createLinearGradient(0, 0, 0, 300);
-            gradientV.addColorStop(0, 'rgba(0,0,0,0)');
-            gradientV.addColorStop(1, '#000');
-            currCanvasCtx.fillStyle = gradientV;
-            currCanvasCtx.fillRect(0, 0, currCanvasCtx.canvas.width, currCanvasCtx.canvas.height); 
+        if (!currCanvasCtx) {
+            return;
         }
-    }, [coreColor]);
+
+        setCanvasCtx(currCanvasCtx);
+            
+        const gradientH = currCanvasCtx.createLinearGradient(0, 0, currCanvasCtx.canvas.width, 0);
+        gradientH.addColorStop(0, '#fff');
+        gradientH.addColorStop(1, new Color('hsv', [hue, 100, 100]).to('srgb').toString());
+        currCanvasCtx.fillStyle = gradientH;
+        currCanvasCtx.fillRect(0, 0, currCanvasCtx.canvas.width, currCanvasCtx.canvas.height);
+
+        const gradientV = currCanvasCtx.createLinearGradient(0, 0, 0, currCanvasCtx.canvas.height);
+        gradientV.addColorStop(0, 'rgba(0,0,0,0)');
+        gradientV.addColorStop(1, '#000');
+        currCanvasCtx.fillStyle = gradientV;
+        currCanvasCtx.fillRect(0, 0, currCanvasCtx.canvas.width, currCanvasCtx.canvas.height);
+    }
+
+    // set color canvas
+    useEffect(() => {
+        updateColorCanvas(color.hsv.h);
+    }, [color]);
 
     // set color slider
     useEffect(() => {
         if (!colorSliderRef.current) return;
 
         const currSliderCtx = colorSliderRef.current.getContext('2d');
-        if (currSliderCtx) {
-            setSliderCtx(currSliderCtx);
-            
-            const gradient = currSliderCtx.createLinearGradient(0, 0, 0, 300);
-            gradient.addColorStop(0, 'rgba(255, 0, 0, 1)');
-            gradient.addColorStop(0.17, 'rgba(255, 255, 0, 1)');
-            gradient.addColorStop(0.34, 'rgba(0, 255, 0, 1)');
-            gradient.addColorStop(0.51, 'rgba(0, 255, 255, 1)');
-            gradient.addColorStop(0.68, 'rgba(0, 0, 255, 1)');
-            gradient.addColorStop(0.85, 'rgba(255, 0, 255, 1)');
-            gradient.addColorStop(1, 'rgba(255, 0, 0, 1)');
-            currSliderCtx.fillStyle = gradient;
-            currSliderCtx.fillRect(0, 0, currSliderCtx.canvas.width, currSliderCtx.canvas.height); 
+        if (!currSliderCtx) {
+            return;
         }
-    }, []);
 
-    useEffect(() => {
-        if (!colorSelectedRef.current) return;
-        colorSelectedRef.current.style.backgroundColor = color;
-    }, [color]);
+        setSliderCtx(currSliderCtx);
+            
+        const gradient = currSliderCtx.createLinearGradient(0, 0, 0, 300);
+        gradient.addColorStop(0, 'rgba(255, 0, 0, 1)');
+        gradient.addColorStop(0.17, 'rgba(255, 255, 0, 1)');
+        gradient.addColorStop(0.34, 'rgba(0, 255, 0, 1)');
+        gradient.addColorStop(0.51, 'rgba(0, 255, 255, 1)');
+        gradient.addColorStop(0.68, 'rgba(0, 0, 255, 1)');
+        gradient.addColorStop(0.85, 'rgba(255, 0, 255, 1)');
+        gradient.addColorStop(1, 'rgba(255, 0, 0, 1)');
+        currSliderCtx.fillStyle = gradient;
+        currSliderCtx.fillRect(0, 0, currSliderCtx.canvas.width, currSliderCtx.canvas.height); 
+    }, []);
 
     // // set color selection coord and draw circle
     // useEffect(() => {
@@ -95,25 +99,36 @@ export function ColorPicker() {
 
     // }, [selectionCoord]);
 
-    function sliderClick(e: React.PointerEvent<HTMLCanvasElement>) {
-        if (!colorSliderRef.current) return;    
-        const rect = colorSliderRef.current.getBoundingClientRect();
-        const imageData = colorSliderRef.current.getContext('2d')!.getImageData(e.clientX - rect.x, e.clientY - rect.y, 1, 1).data;
-        const rgbaColor = 'rgba(' + imageData[0] + ',' + imageData[1] + ',' + imageData[2] + ',1)';
-        setCoreColor(rgbaColor);        
+    function sliderClick() {
+        if (!colorSliderRef.current) return;
+
+        // set color chosen
+        const newHue = (hueY / colorSliderRef.current.height) * 360;
+        const newColor = new Color(color);
+        newColor.hsv.h = newHue;
+        updateColor(newColor);
+
+        // update selected color tile
+        if (!colorSelectedRef.current) return;
+        colorSelectedRef.current.style.backgroundColor = newColor.to('srgb').toString();
     }
 
     function handleCanvasInteract() {
-        if (!isDown || !colorCanvasRef.current) return;
+        if (!isColorDown || !colorCanvasRef.current) return;
 
         // set new coordinates to draw indicator
         // const newCoord : Coord = {x : e.clientX - rect.x, y : e.clientY - rect.y};
         // setSelectionCoord(newCoord);
         
         // set color chosen
-        const imageData = colorCanvasRef.current.getContext('2d', {willReadFrequently: true})!.getImageData(pointerX, pointerY, 1, 1).data;
-        const rgbaColor = 'rgba(' + imageData[0] + ',' + imageData[1] + ',' + imageData[2] + ',1)';
-        updateColor(rgbaColor);  
+        const saturation = colorX / colorCanvasRef.current.width;
+        const value = colorY / colorCanvasRef.current.height;
+        const newColor = new Color('hsv', [color.hsl.h, saturation * 100, (1 - value) * 100]);
+        updateColor(newColor);
+
+        // update selected color tile
+        if (!colorSelectedRef.current) return;
+        colorSelectedRef.current.style.backgroundColor = newColor.to('srgb').toString();
     }
 
     return (
--- src/contexts/color-context.tsx
+++ src/contexts/color-context.tsx
@@ -1,22 +1,23 @@
 import React, {createContext, useState} from 'react';
+import Color from 'colorjs.io';
 
 interface ColorContextData {
-  color: string;
+  color: Color;
   // eslint-disable-next-line no-unused-vars
-  updateColor: (newColor : string) => void;
+  updateColor: (newColor : Color) => void;
 }
 
 export const ColorContext = createContext<ColorContextData>( {
-    color: 'black',
+    color: new Color('black'),
     // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
-    updateColor: (newColor : string) => {}
+    updateColor: (newColor : Color) => {}
 });
 
 // annotating props instead of using ReactFC to indicate typing
 export const ColorProvider = (props: { children : React.ReactNode } ) => {
-    const [color, setColor] = useState('black');
+    const [color, setColor] = useState(new Color('black'));
 
-    const updateColor = (newColor : string) => {
+    const updateColor = (newColor : Color) => {
         setColor(newColor);
     };
 
