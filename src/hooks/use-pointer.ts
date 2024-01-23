6bfa0f1f872b9cabccad6a11f948324b83140333
Vincent Sit <vincentsit00@ymail.com>
2024-01-24 01:13:19 +1300

    Add clamping function to pointer hook

--- src/hooks/use-pointer.ts
+++ src/hooks/use-pointer.ts
@@ -1,4 +1,4 @@
-import { RefObject, useEffect, useState } from 'react';
+import { RefObject, useEffect, useState, useRef } from 'react';
 
 type PointerState = {
     isDown: boolean;
@@ -13,31 +13,77 @@ export function usePointer(ref?: RefObject<HTMLElement>) {
         y: 0,
     });
 
+    const isWithinRef = useRef(false);
+
     useEffect(() => {
         if (ref && !ref.current) {
             return;
         }
         
         // If no ref passed, default to document.body
-        const element = ref?.current ? ref.current : document.body;
+        const element = ref?.current ? ref.current : document.body;                
 
-        const handlePointer = (e: PointerEvent) => {
+        const calculateCoordWithinBounds = (e: PointerEvent) => {
             const rect = element.getBoundingClientRect();
+
+            if (isWithinBounds(e)) {
+                return {
+                    x: e.clientX - rect.x,
+                    y: e.clientY - rect.y
+                };
+            }
+
+            const newX = e.clientX > rect.right ? rect.width : e.clientX < rect.x ? 0 : e.clientX - rect.x;
+            const newY = e.clientY > rect.bottom ? rect.height : e.clientY < rect.y ? 0 : e.clientY - rect.y;
+            return {
+                x : newX,
+                y : newY
+            };
+        };
+
+        const isWithinBounds = (e: PointerEvent) => {
+            const rect = element.getBoundingClientRect();            
+            return (e.clientX >= rect.left && e.clientX <= rect.right && e.clientY >= rect.top && e.clientY <= rect.bottom);
+        };
+
+        const handleDown = (e: PointerEvent) => {                                    
+            if (!isWithinBounds(e)) return;                    
+
+            isWithinRef.current = true;
+            
+            const rect = element.getBoundingClientRect();            
+            
             setState({
                 isDown: e.pressure > 0,
                 x: e.clientX - rect.x,
-                y: e.clientY - rect.y,
+                y: e.clientY - rect.y
             });
         };
 
-        element.addEventListener('pointerdown', handlePointer);
-        element.addEventListener('pointerup', handlePointer);
-        element.addEventListener('pointermove', handlePointer);
+        const handleUp = () => {
+            if (isWithinRef.current) {
+                isWithinRef.current = false;
+            }
+        };
+
+        const handlePointer = (e: PointerEvent) => {            
+            if (!isWithinRef.current) return;            
+            const newCoord = calculateCoordWithinBounds(e);            
+            setState({
+                isDown: e.pressure > 0,
+                x: newCoord.x,
+                y: newCoord.y
+            });            
+        };
+
+        document.body.addEventListener('pointerdown', handleDown);
+        document.body.addEventListener('pointerup', handleUp);
+        document.body.addEventListener('pointermove', handlePointer);
 
         return () => {
-            element.removeEventListener('pointerdown', handlePointer);
-            element.removeEventListener('pointerup', handlePointer);
-            element.removeEventListener('pointermove', handlePointer);
+            document.body.removeEventListener('pointerdown', handlePointer);
+            document.body.removeEventListener('pointerup', handlePointer);
+            document.body.removeEventListener('pointermove', handlePointer);
         };
     }, []);
 
