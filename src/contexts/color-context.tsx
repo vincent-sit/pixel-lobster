772ed298e98d7431b6331674731634a03cb3c7d7
Vincent Sit <vincentsit00@ymail.com>
2024-01-24 22:31:54 +1300

    Adjust initial color to match coordinate

--- src/contexts/color-context.tsx
+++ src/contexts/color-context.tsx
@@ -8,14 +8,14 @@ interface ColorContextData {
 }
 
 export const ColorContext = createContext<ColorContextData>( {
-    color: new Color('black'),
+    color: new Color('white'),
     // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
     updateColor: (newColor : Color) => {}
 });
 
 // annotating props instead of using ReactFC to indicate typing
 export const ColorProvider = (props: { children : React.ReactNode } ) => {
-    const [color, setColor] = useState(new Color('black'));
+    const [color, setColor] = useState(new Color('white'));
 
     const updateColor = (newColor : Color) => {
         setColor(newColor);
