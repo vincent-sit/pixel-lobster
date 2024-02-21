d1c011e8258d266f38059c6de0f136b4ff4af0ad
Vincent Sit <vincentsit00@ymail.com>
2024-02-21 19:44:30 +1300

    Add function property to ButtonProps and create two new functions

--- src/components/command-panel/Command-Panel.tsx
+++ src/components/command-panel/Command-Panel.tsx
@@ -14,9 +14,29 @@ const Wrapper = styled.div`
 `;
 
 export function CommandPanel() {
+    function clearCanvas() {
+        const canvas = document.getElementById('drawing-canvas');
+        if (!canvas || !(canvas instanceof HTMLCanvasElement)) return;
+        const context = canvas.getContext('2d');
+        context?.clearRect(0, 0, canvas.width, canvas.height);
+    }
+
+    function exportCanvas() {
+        const canvas = document.getElementById('drawing-canvas');
+        if (!canvas || !(canvas instanceof HTMLCanvasElement)) return;
+        const canvasUrl = canvas.toDataURL();
+        const createEl = document.createElement('a');
+        createEl.href = canvasUrl;
+            
+        createEl.download = 'pixel-lobster-art';
+        createEl.click();
+        createEl.remove();
+    }
+
     return (
         <Wrapper>
-            <CommandPanelButton text='Clear Canvas' id='clearCanvas'/>
+            <CommandPanelButton text='Clear Canvas' id='clear-canvas-button' function={clearCanvas}/>
+            <CommandPanelButton text='Export Canvas' id='export-canvas-button' function={exportCanvas}/>
         </Wrapper>
     );
 }
