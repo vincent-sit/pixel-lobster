48571cc7ceb6a250a5dbbee962806c5efa976066
Vincent Sit <vincentsit00@ymail.com>
2024-02-22 13:41:08 +1300

    Update tools data file name

--- src/components/tool-bar/tool-set.tsx
+++
@@ -1,8 +0,0 @@
-import { TOOL } from '../../contexts/tool-context';
-import paintBrushUrl from '../../assets/paintbrush.png';
-
-export const ToolSet = [
-    {tool: TOOL.PAINTBRUSH, imageLink: paintBrushUrl, altText: 'Paint Brush'},
-    {tool: TOOL.ERASER, imageLink: paintBrushUrl, altText: 'Eraser'},
-    {tool: TOOL.COLORPICKER, imageLink: paintBrushUrl, altText: 'Color Picker'},
-];
