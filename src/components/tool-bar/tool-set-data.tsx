4e431cf261d9bae747258981367924ba2a3df22c
Vincent Sit <vincentsit00@ymail.com>
2024-02-24 07:56:03 +1300

    Add assets - button images

--- src/components/tool-bar/tool-set-data.tsx
+++ src/components/tool-bar/tool-set-data.tsx
@@ -4,7 +4,7 @@ import eraserUrl from '../../assets/eraser.png';
 import pickerUrl from '../../assets/colorPicker.png';
 
 export const ToolSetData = [
-    {tool: TOOL.PAINTBRUSH, imageLink: paintBrushUrl, altText: 'Paint Brush'},
-    {tool: TOOL.ERASER, imageLink: eraserUrl, altText: 'Eraser'},
-    {tool: TOOL.COLORPICKER, imageLink: pickerUrl, altText: 'Color Picker'},
+    {tool: TOOL.PAINTBRUSH, imageLink: paintBrushUrl, altText: 'Paint Brush', isPressed: true},
+    {tool: TOOL.ERASER, imageLink: eraserUrl, altText: 'Eraser', isPressed: false},
+    {tool: TOOL.COLORPICKER, imageLink: pickerUrl, altText: 'Eye Dropper', isPressed: false},
 ];
